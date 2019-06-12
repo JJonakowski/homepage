42abab3e9659a9826934da882807fde4035b0a05
Jakub JJ <jakubjonakowski@gmail.com>
2019-06-12 22:59:24 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const name='Jakub';
+const age=36;
+
+console.log(`mam na imię ${name} i mam ${age} lat`);
---
+++ test/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>xxxxxxxxxxxxxxxxxxxxxxx</title>
+  <link rel="stylesheet" type="text/css" href="style-zad2.css">
+</head>
+
+<body>
+<div class="header">
+  <h1> HEADER </h1>
+
+  <div class="content">
+  <h2>Duży nagłówek</h2>
+  <p>Paragraf oraz <a href="//www.google.pl/" target="_blank">gugle</a>.</p>
+  <img src="1.jpg" alt="1.jpg" title="bohomazy" width="auto">
+  <br>
+  <p>Ka<sup>s</sup>ia</p>
+
+  <a href="#sekcja1"> Na sam dół</a>
+
+  </div>
+</div>
+
+<div class="content">
+  <iframe src="index1.php" width="100%" height="800px"></iframe>
+</div>
+
+<!--
+<p id="sekcja1">
+  <img src="nagore.jpg" target="www.google.com" href="www.google.com">
+-->
+
+<p id="sekcja1">
+  przechodzimy z gory na dol
+</p>
+
+
+
+<div class="footer">
+  <p> jakubek&nbsp;copyright&nbsp;testtttttt&nbsp;&reg;</p>
+</div>
+
+<br><br><br><br><br><br><br><br><br><br><br><br>
+
+</body>
+</html>
