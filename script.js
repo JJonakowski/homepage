b786b3e7d3f10cd957b84d96f9e922e1c237abcf
Jakub JJ <jakubjonakowski@gmail.com>
2019-06-15 18:32:57 +0200

    uporzadkowane klasy

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const name='Jakub';
 const age=36;
 
 console.log(`mam na imię ${name} i mam ${age} lat`);
+
+const main__header = document.querySelector('.main__header');
+console.log(main__header.innerHTML);
+main__header.innerHTML = `<i>mam</i> na imię ${name} i mam ${age} lat`
+
+const section = document.querySelectorAll('section');
+console.log(section);
+
+console.log(section[1]);
