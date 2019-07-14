007ff7229cfb7d7adb7e109819e58088d7dc1d3a
Jakub JJ <jakubjonakowski@gmail.com>
2019-07-14 18:53:00 +0200

    feat: grid / hamburger menu

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-const name='Jakub';
-const age=36;
-console.log(`mam na imię ${name} i mam ${age} lat`);
+const button = document.querySelector('.header__button--js'); //wybieramy button
 
-//const main__header = document.querySelector('.main__header');
-//console.log(main__header);
-//console.log(main__header.innerHTML);
-//main__header.innerHTML = `<i>mam</i> na imię ${name} i mam ${age} lat`
-
-//const section = document.querySelectorAll('section');
-//console.log(section);
-//console.log(section[1]);
-
-//const about = document.querySelector('.main__who-me');
-//about.innerHTML = `xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`;
-
-function kalkulator (x) {
-x=x+1;
-console.log(x);
-return x*2;
+function klik() {
+    console.log('halo');
 }
 
-console.log(kalkulator(2));
-//const licz = kalkulator(2);
-//console.log(licz);
+//button.addEventListener('click', klik); //dodajemy listenera dzialajacego na klikniecie z funkcja
 
-const calc = (x) => {
-x=x+2;
-console.log(x);
-return x*2;
-}
+const main = document.querySelector('.main__who-me--js');
 
-console.log(calc(1));
+button.addEventListener('click', (e) => { //e pokazuje wszystkie szczegoly elementu
+    console.log(e);
+    main.innerHTML = `klik klik`;
+//  main.classList.add('main__who-me--red'); //zmienia kolor naglowka na czerwony
+     main.classList.toggle('main__who-me--red'); // kazde klikniecie powoduje zmiene koloru czarny-czerwony-czarny
+//if (main.classList.contains('main__who-me--red')) { //zwraca tru/false 
+//    console.log('jest klasa');
+//} else {
+//   console.log('klasy nie ma');
+//}
+})
 
-const calcul = x => (x*2);
-console.log(calcul(1));
+const navigationSwitch = document.querySelector('.navigation__switch--js');
+
+
+navigationSwitch.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
